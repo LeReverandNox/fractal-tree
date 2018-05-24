b926d8c17cf840ddaa033d70ed33d79c3aba9e54
lereverandnox <lereverandnox@gmail.com>
2018-05-25 01:21:37 +0200

    Can now move the tree by clicking, and support a mode with multiple trees. EXPERIMENTAL

--- src/playground/fractal_tree/sketch.js
+++ src/playground/fractal_tree/sketch.js
@@ -1,6 +1,6 @@
 const DEF_WIDTH = window.innerWidth - 10;
 const DEF_HEIGHT = window.innerHeight - 10;
-const DEF_BG_COLOR = 51;
+const DEF_BG_COLOR = [202, 8, 19];
 const DEF_TREE_START_X = DEF_WIDTH / 2;
 const DEF_TREE_START_Y = DEF_HEIGHT;
 const DEF_TREE_START_ANGLE = 180;
@@ -26,17 +26,31 @@ let branchesCoef;
 let branchesNb;
 let branchesAngle;
 
-let tree;
+let moveMode = true;
+let trees = [];
 
 function setup() {
     createCanvas(DEF_WIDTH, DEF_HEIGHT);
     reset_settings();
-    init()
+    newTree();
 }
 
-function init() {
+function updateTrees() {
+    for (let i = 0; i < trees.length; i += 1) {
+        // console.log(`UPDATING TREE #${i}`);
+        let treeStartingPoint = moveMode ? createVector(treeStartX, treeStartY) : trees[i].startingVector;
+        trees[i] = new Tree(treeColorMode, treeDepth, trunkLength, trunkThickness, branchesCoef, branchesNb, branchesAngle, treeStartAngle, treeStartingPoint);
+    }
+}
+
+function newTree() {
     let treeStartingPoint = createVector(treeStartX, treeStartY);
-    tree = new Tree(treeColorMode, treeDepth, trunkLength, trunkThickness, branchesCoef, branchesNb, branchesAngle, treeStartAngle, treeStartingPoint);
+    let tree = new Tree(treeColorMode, treeDepth, trunkLength, trunkThickness, branchesCoef, branchesNb, branchesAngle, treeStartAngle, treeStartingPoint);
+    trees.push(tree);
+}
+
+function keepOneTree() {
+    trees.length = 1;
 }
 
 function reset_settings() {
@@ -53,100 +67,123 @@ function reset_settings() {
     branchesCoef = DEF_BRANCHES_COEF;
     branchesNb = DEF_BRANCHES_NB;
     branchesAngle = DEF_BRANCHES_ANGLE;
+
+    moveMode = true;
 }
 
 function draw() {
-    background(DEF_BG_COLOR);
+    background(...DEF_BG_COLOR);
 
-    tree.show();
+    for (let tree of trees) {
+        tree.show();
+    }
 }
 
 function keyPressed() {
-    console.log(keyCode);
+    // console.log(keyCode);
     switch (keyCode) {
         // UP
         case 38:
             branchesAngle *= 1.05;
-            init();
+            updateTrees();
             break;
         // DOWN
         case 40:
             branchesAngle *= 0.95;
-            init();
+            updateTrees();
             break;
         // LEFT
         case 37:
             branchesCoef *= 0.95;
-            init();
+            updateTrees();
             break;
         // RIGHT
         case 39:
             branchesCoef *= 1.05;
-            init();
+            updateTrees();
             break;
         // +
         case 187:
             branchesNb += 1;
-            init();
+            updateTrees();
             break;
         // -
         case 189:
             branchesNb -= 1;
-            init();
+            updateTrees();
             break;
         // [
         case 219:
             treeDepth -= 1;
-            init();
+            updateTrees();
             break;
         // ]
         case 221:
             treeDepth += 1;
-            init();
+            updateTrees();
             break;
         // 9
         case 57:
             trunkLength *= 0.95;
-            init();
+            updateTrees();
             break;
         // 0
         case 48:
             trunkLength *= 1.05;
-            init();
+            updateTrees();
             break;
         // SPACE
         case 32:
             treeColorMode = treeColorMode === Tree.RGB ? Tree.BW : Tree.RGB;
-            init();
+            updateTrees();
             break;
         // A
         case 65:
             treeStartAngle -= 1;
-            init();
+            updateTrees();
             break;
         // D
         case 68:
             treeStartAngle += 1;
-            init();
+            updateTrees();
             break;
         // Q
         case 81:
             trunkThickness -= 1;
-            init();
+            updateTrees();
             break;
         // E
         case 69:
             trunkThickness += 1;
-            init();
+            updateTrees();
             break;
         // R
         case 82:
             reset_settings();
-            init();
+            keepOneTree();
+            updateTrees();
+            break;
+        // M
+        case 77:
+            moveMode = !moveMode;
+            if (moveMode) {
+                keepOneTree();
+            }
+            updateTrees();
             break;
         default:
             break;
     }
 
     return false;
+}
+
+function mouseClicked() {
+    treeStartX = mouseX;
+    treeStartY = mouseY;
+    if (moveMode) {
+        updateTrees();
+    } else {
+        newTree();
+    }
 }
